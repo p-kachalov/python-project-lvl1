e8c3d9d2040325983739316f2ba4ad12e3e78e81
Pavel Kachalov <pa.kachalov@gmail.com>
2020-06-30 00:15:53 +0300

    custom calculator

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,4 +1,5 @@
 """Brain Calc game logic."""
+import operator
 import random
 from typing import Tuple
 
@@ -6,6 +7,28 @@ MIN_NUMBER = 0
 MAX_NUMBER = 10
 
 DESCRIPTION = 'What is the result of the expression?'
+
+
+def calculate(operand1: int, operand2: int, operator_sign: str) -> int:
+    """
+    Calculate the result of applying the operation to operands.
+
+    Args:
+        operand1: first operand
+        operand2: second operand
+        operator_sign: operator to apply
+
+    Returns:
+        int: result of calculation
+    """
+    operator_name = {
+        '+': 'add',
+        '-': 'sub',
+        '*': 'mul',
+    }
+
+    operator_function = getattr(operator, operator_name[operator_sign])
+    return operator_function(operand1, operand2)
 
 
 def get_task() -> Tuple[str, str]:
@@ -17,14 +40,14 @@ def get_task() -> Tuple[str, str]:
     """
     number1 = random.randint(MIN_NUMBER, MAX_NUMBER)
     number2 = random.randint(MIN_NUMBER, MAX_NUMBER)
-    operator = random.choice(['+', '-', '*'])
+    operator_sign = random.choice(['+', '-', '*'])
 
     question = '{number1} {operator} {number2}'.format(
         number1=number1,
         number2=number2,
-        operator=operator,
+        operator=operator_sign,
     )
 
-    answer = str(eval(question))  # noqa: S307 WPS421
+    answer = str(calculate(number1, number2, operator_sign))
 
     return question, answer
