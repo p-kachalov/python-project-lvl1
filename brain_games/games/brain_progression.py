cb910ced6e88942718d91e2d8a26aedfe5511398
Pavel Kachalov <pa.kachalov@gmail.com>
2020-06-29 23:27:07 +0300

    purer progression function

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -9,17 +9,22 @@ MAX_STEP = 10
 DESCRIPTION = 'What number is missing in the progression?'
 
 
-def make_progression() -> list:
+def make_progression(min_step, max_step, progression_len) -> list:
     """
     Make random arithmetic progression.
+
+    Args:
+        min_step: minimal step value
+        max_step: maximal step value
+        progression_len: length of progression
 
     Returns:
         a list of numbers an arithmetic progression
     """
-    start = random.randint(MIN_STEP, PROGRESSIONN_LEN)  # noqa: S311
-    step = random.randint(MIN_STEP, MAX_STEP)  # noqa: S311
+    start = random.randint(min_step, max_step)  # noqa: S311
+    step = random.randint(min_step, max_step)  # noqa: S311
 
-    return [num * step for num in range(start, start+PROGRESSIONN_LEN)]
+    return [num * step for num in range(start, start+progression_len)]
 
 
 def get_task() -> Tuple[str, str]:
@@ -29,7 +34,11 @@ def get_task() -> Tuple[str, str]:
     Returns:
         dict: dict with question and answer
     """
-    progression = make_progression()
+    progression = make_progression(
+        min_step=MIN_STEP,
+        max_step=MAX_STEP,
+        progression_len=PROGRESSIONN_LEN,
+    )
     index = random.randint(0, PROGRESSIONN_LEN)  # noqa: S311
     hide_number = progression[index]
     progression[index] = '...'
